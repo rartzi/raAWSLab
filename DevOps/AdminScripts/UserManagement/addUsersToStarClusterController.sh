f10e25e9bbc5abf2fe49394b1cfe06a3068103ad
Ronen Artzi <ronen.artzi@gmail.com>
2014-09-23 23:08:57 -0400

    Work on introducing Gluster and adding external collaborators to the starcluster work

--- DevOps/AdminScripts/UserManagement/addUsersToStarClusterController.sh
+++ DevOps/AdminScripts/UserManagement/addUsersToStarClusterController.sh
@@ -14,7 +14,7 @@ if [ $# -lt 2 ]; then
 	echo ""	
 	echo " Usage:   sudo $0 UserID "
 	echo ""
-	echo " example : sudo  $0  Ronen ./Test/TestKey.pub" 
+	echo " example : sudo  $0  Ronen TestKey.pub" 
 	echo "-------------------------------------------------------"
 	echo " This script is meant to be run on an EC2instance and add a user and it's ssh public key."
 	echo " running the script, the use will be able to ssh into the machine with his key."
@@ -26,15 +26,23 @@ if [ $# -lt 2 ]; then
   exit 1
 fi
 user=$1
-path_to_public_key=$2
+public_key_name=$2
 echo $user
-echo $path_to_public_key
+echo $public_key_name
 adduser $user
+cd /home/$user
+cp -R /home/ec2-user/.starcluster /home/$user/.starcluster
 mkdir /home/$user/.ssh
-chmod 700 /home/$user/.ssh
-aws s3 cp s3://RDI-DevOps/PublicKeys/$user.pub
-cat $user.pub> /home/$user/.ssh/authorized_keys
+cp /home/ec2-user/.ssh/*.rsa /home/$user/.ssh/
+curl -o $public_key_name https://s3.amazonaws.com/RDI-DevOps/PublicKeys/$public_key_name
+cat $public_key_name > /home/$user/.ssh/authorized_keys
+chown $user:ec2-user -R /home/$user/.ssh/authorized_keys
+chown $user:ec2-user -R /home/$user/.ssh
+chown $user:ec2-user -R /home/$user/.starcluster
 chmod 600 /home/$user/.ssh/authorized_keys
-chown $user /home/$user/.ssh/authorized_keys
-chown $user:ec2-user /home/$user/.ssh
+chmod 700 /home/$user/.ssh/
+
+
+
+
 echo "Done: check that you can ssh as $user with your private key" 
---
+++ DevOps/AdminScripts/UserManagement/add_starcluster_controller_ssh_user-key-in-s3.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+#  This script is meant to be run on an EC2instance and add a user and it's ssh public key.
+#  for example ec2-user / ubuntu
+#  running the script, the use will be able to ssh into the machine with his key.
+#
+#  Assumptions:
+#			RSA key pairs were created ( ssh-keygen -t rsa )
+# 			This script and the public Key files were uploaded to the initial instance user
+# 			Run the script under sudo 
+#
+if [ $# -lt 2 ]; then
+	echo ""
+	echo ""	
+	echo " Usage:   sudo $0 UserID "
+	echo ""
+	echo " example : sudo  $0  Ronen TestKey.pub" 
+	echo "-------------------------------------------------------"
+	echo " This script is meant to be run on an EC2instance and add a user and it's ssh public key."
+	echo " running the script, the use will be able to ssh into the machine with his key."
+	echo "" 
+	echo "Assumptions:"
+	echo "RSA key pairs were created ( ssh-keygen -t rsa )"
+	echo "user public key is transfered to S3 bucket:  /RDI-DevOps/PublickKeys/userid.pub and made public"
+	echo "Run the script under sudo " 
+  exit 1
+fi
+user=$1
+public_key_name=$2
+echo $user
+echo $public_key_name
+adduser $user
+cd /home/$user
+cp -R /home/ec2-user/.starcluster /home/$user/.starcluster
+mkdir /home/$user/.ssh
+cp /home/ec2-user/.ssh/*.rsa /home/$user/.ssh/
+curl -o $public_key_name https://s3.amazonaws.com/RDI-DevOps/PublicKeys/$public_key_name
+cat $public_key_name > /home/$user/.ssh/authorized_keys
+chown $user:ec2-user -R /home/$user/.ssh/authorized_keys
+chown $user:ec2-user -R /home/$user/.ssh
+chown $user:ec2-user -R /home/$user/.starcluster
+chmod 600 /home/$user/.ssh/authorized_keys
+chmod 700 /home/$user/.ssh/
+
+
+
+
+echo "Done: check that you can ssh as $user with your private key" 
