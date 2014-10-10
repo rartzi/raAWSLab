8d865adb530ad04affd67cb89bf7ad02075085e0
Ronen Artzi <ronen.artzi@gmail.com>
2014-10-10 07:20:13 -0400

    bug fix

--- DevOps/AdminScripts/UserManagement/addUsersToStarClusterController.sh
+++ DevOps/AdminScripts/UserManagement/addUsersToStarClusterController.sh
@@ -33,16 +33,14 @@ adduser $user
 cd /home/$user
 cp -R /home/ec2-user/.starcluster /home/$user/.starcluster
 mkdir /home/$user/.ssh
-cp /home/ec2-user/.ssh/*.rsa /home/$user/.ssh/
-curl -o $public_key_name https://s3.amazonaws.com/RDI-DevOps/PublicKeys/$public_key_name
+cp /home/ec2-user/.ssh/*.rsa /home/$user/.ssh/ 
+cp /home/ec2-user/.ssh/*.pem /home/$user/.ssh/
+curl -o $public_key_name https://s3.amazonaws.com/rdi-devops/PublicKeys/$public_key_name
 cat $public_key_name > /home/$user/.ssh/authorized_keys
+rm $public_key_name
 chown $user:ec2-user -R /home/$user/.ssh/authorized_keys
 chown $user:ec2-user -R /home/$user/.ssh
 chown $user:ec2-user -R /home/$user/.starcluster
 chmod 600 /home/$user/.ssh/authorized_keys
 chmod 700 /home/$user/.ssh/
-
-
-
-
 echo "Done: check that you can ssh as $user with your private key" 
